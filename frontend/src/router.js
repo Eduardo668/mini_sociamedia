3bde2f59aa7e9492f052b95bea46bdad94bd409a
Eduardo668 <78269439+Eduardo668@users.noreply.github.com>
2022-06-04 13:40:03 -0300

    First Commit

---
+++ frontend/src/router.js
@@ -0,0 +1,9 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+const Router = ()=>{
+    <BrowserRouter>
+        <Routes>
+            <Route path='/login' element />
+        </Routes>
+    </BrowserRouter>
+}
